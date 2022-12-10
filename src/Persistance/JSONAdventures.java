6acfca061d2afbbf0a3563a6e1d277eb910e4b58
ignasisanchez <ignasisanchez06@gmail.com>
2022-12-10 19:27:07 +0100

    fet part de json

--- src/Persistance/JSONAdventures.java
+++ src/Persistance/JSONAdventures.java
@@ -1,4 +1,49 @@
 package Persistance;
 
+import Business.Entities.Adventure;
+import com.google.gson.Gson;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileReader;
+import java.util.Iterator;
+
+
+/*      FORMAT JSON ADVENTURES
+
+[
+    {
+        "name": "AventureName",
+        "combats": "4",
+        "monsters": 2, // Minim 3 personatges
+    },
+]*/
+
 public class JSONAdventures {
+
+    File jsonAdventuresFile = new File("Files/JSONAdventuresFile.json");
+
+
+    public void saveAdventuresToFile(ArrayList<Adventure> adventuresList) {
+        try {
+            FileWriter f = new FileWriter(jsonAdventuresFile ,false);
+            f.write(new Gson().toJson(adventuresList));
+            f.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Adventure> getAdventuresFromFile (){
+
+    }
+
+
+
+
 }
--- src/Persistance/JSONCharacters.java
+++ src/Persistance/JSONCharacters.java
@@ -1,4 +1,90 @@
 package Persistance;
 
+import Business.Entities.Character;
+import Business.Entities.Monster;
+import com.google.gson.Gson;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileReader;
+import java.util.Iterator;
+
+/*      FORMAT JSON CHARACTERS
+[
+    {
+        "name": "Amrod",
+        "player": "SonsOfFeanor",
+        "xp": 229,
+        "body": 2,
+        "mind": 0,
+        "spirit": 0,
+        "class": "Adventurer"
+
+    },
+    {
+        "name": "Caranthir",
+        "player": "SonsOfFeanor",
+        "xp": 306,
+        "body": 2,
+        "mind": 0,
+        "spirit": 1,
+        "class": "Adventurer"
+    }
+]
+ */
+
 public class JSONCharacters {
+
+    File jsonCharactersFile = new File("Files/JSONCharactersFile.json");
+
+    public void savCharactersToFile(ArrayList<Character> charactersList) {
+        try {
+            FileWriter f = new FileWriter(jsonCharactersFile ,false);
+            f.write(new Gson().toJson(charactersList));
+            f.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Character> getCharactersFromFile (){
+        ArrayList<Character> characterList = new ArrayList<>();
+        JSONParser jsonParser = new JSONParser();
+
+        try {
+
+            Object obj = jsonParser.parse(new FileReader(jsonCharactersFile));
+            JSONArray characterJSONList = (JSONArray) obj;
+
+            Iterator<JSONObject> characterListJSON_iterator = characterJSONList.iterator();
+
+            while (characterListJSON_iterator.hasNext()){
+
+                JSONObject object = characterListJSON_iterator.next();
+                Character character = new Character();
+
+
+                character.setName(object.get("name").toString());
+                // TODO: FALTA ABAIX
+                character.setChallenge(object.get("challenge").toString());
+                character.setExperience(Integer.parseInt(object.get("experience").toString()));
+                character.setExperience(Integer.parseInt(object.get("hitPoints").toString()));
+                character.setExperience(Integer.parseInt(object.get("initiative").toString()));
+                character.setChallenge(object.get("damageDice").toString());
+                character.setChallenge(object.get("Physical").toString());
+
+                characterList.add(character);
+            }
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        return characterList;
+    }
 }
